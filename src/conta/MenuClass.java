ed50dab007c750470dc89be2c1c1decb735a404e
Raul-Prado <rauloliveiraprado@gmail.com>
2024-03-04 21:50:35 -0300

    Projeto Conta Bancaria

---
+++ src/conta/MenuClass.java
@@ -0,0 +1,14 @@
+package conta;
+
+public class MenuClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		mensagem();
+	}
+	
+	public static void mensagem() {
+		System.out.println("Teste");
+	}
+
+}
